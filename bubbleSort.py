d04d7cb9de8dbf498675edc52aba5c3f41e5524a
Hementh-BV <107341439+Hementh-BV@users.noreply.github.com>
2022-09-20 19:28:19 +0530

    Create bubbleSort.py

---
+++ bubbleSort.py
@@ -0,0 +1,12 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+
+arr = [20, 48, 52, 12, 30, 44, 80]
+print(bubble_sort(arr))
